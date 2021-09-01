a5b0c4d4a93facf071118a20b44df57e6e858ef6
HP <HP@DESKTOP-N5QBMVO>
2021-09-01 14:16:10 +0530

    Add project files.

---
+++ Variables/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+			/*int num = 100;
+			float rate = 10.2f;
+			decimal amount = 100.50M;
+			char code = 'C';
+			bool isValid = true;
+			string name = "Steve";
+
+			Console.WriteLine(num);
+			Console.WriteLine(rate);
+			Console.WriteLine(amount);
+			Console.WriteLine(code);
+			Console.WriteLine(isValid);
+			Console.WriteLine(name);
+			Console.ReadLine();*/
+
+			bool result;
+			int x = 10;
+			result = (!(x == 10));
+			Console.WriteLine(result);
+			Console.ReadLine();
+
+
+		}
+	}
+}
